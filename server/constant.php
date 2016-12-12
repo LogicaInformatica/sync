7a5f232a6a93e85fa17c2a5f8e584d7a52152187
LOGICADOMAIN\gdifalco <g.difalco@logicainformatica.it>
2016-12-12 15:47:26 +0100

    Pulita constant.php

--- server/constant.php
+++ server/constant.php
@@ -4,57 +4,24 @@
 //            quindi mettete fuori dallo "switch" sottostante solo le costanti che sono identiche in tutti gli ambienti
 //            e dentro i case dello "switch" quelle che si differenziano almeno per uno degli ambienti
 //
-$sito = 'PROD';
+$sito = $_SERVER['ENVIRONMENT'];
 switch ($sito) {
-	case 'LOCAL':
-		define('SITE_NAME', 'Conn@&cut Local');
-		define('MYSQL_SERVER', '77.43.16.58');
-		define('MYSQL_USER',   'cnclabit');
-		define('MYSQL_PASS',   'cnclabit');
-		define('MYSQL_SCHEMA', 'db_cnc');
-		define('MYSQL_PORT',   '3306');
-		define('PORTAL_URL',   'http://localhost:4001/DCS/');
-		define('LINK_URL',   'http://localhost:4001/DCS/');
-		define('SMS_TEST_NR','dummy'); // no sms
-		define('MAIL_TEST', "dummy"); // indica che l'invio mail non viene fatto, in realt�
-		define('MAIL_OVERRIDE','dummy');
-		define('ADMIN_MAIL',"dummy"); // mail dell'amministratore di sistema
-		define('MEN_AT_WORK_PAGE',""); // pagina di avviso per interruzione servizio. Se impostata (es. Avviso.html) non fa entrare nell'app ma rimanda ad essa
-		define('TEXT_NEWLINE', "\n");  // salto linea per le righe delle lettere formato testo semplice
-		define('LOGO_PRODOTTO','<img src="images/logo conn@&cut.jpg">');
-		define('LOGO_SOCIETA','<img src="images/logo TFSI.gif">');
-		define('FOOTER','<div style="text-align:center;font-size:10px;color:gray">&copy; 2011-2015 Toyota Financial Services (UK) PLC - P.IVA 05303901002</div>');
-		break;
-	case 'LABIT':
+	case 'lbit':
 		define('SITE_NAME', 'Conn@&cut LabIt');
-		define('MYSQL_SERVER', '127.0.0.1');
-		define('MYSQL_USER',   'cnclabit');
-		define('MYSQL_PASS',   'cnclabit');
-		define('MYSQL_SCHEMA', 'db_cnc');
-		define('MYSQL_PORT',   '3306');
-		define('PORTAL_URL',   'https://portallabit.tfsi.it/');
-		define('LINK_URL',   'https://cnclabit.tfsi.it/');
 		define('SMS_TEST_NR','dummy'); // no sms
 		define('MAIL_TEST', "g.difalco@logicainformatica.it"); // indica che l'invio mail non viene fatto, in realt�
 		define('MAIL_OVERRIDE','g.difalco@logicainformatica.it');
 		define('ADMIN_MAIL',"g.difalco@logicainformatica.it"); // mail dell'amministratore di sistema
-		define('MEN_AT_WORK_PAGE',""); // pagina di avviso per interruzione servizio. Se impostata (es. Avviso.html) non fa entrare nell'app ma rimanda ad essa
+		define('MEN_AT_WORK_PAGE',""); // pagina di avviso per interruzione servizio. Se impostata (es. Avviso.html) non fa entrare nell'app ma rima$
 		define('TEXT_NEWLINE', "\r\n");  // salto linea per le righe delle lettere formato testo semplice
 		define('LOGO_PRODOTTO','<img src="images/logo conn@&cut.jpg">');
 		define('LOGO_SOCIETA','<img src="images/nuovo_logo_TFSI.png">');
 		define('FOOTER','<div style="text-align:center;font-size:10px;color:gray">&copy; 2011-2015 Toyota Financial Services (UK) PLC - P.IVA 05303901002</div>');
-		break;
-	case 'TEST':
+        break;
+	case 'test':
 		define('SITE_NAME', 'Conn@&cut Test');
-		define('MYSQL_SERVER', '192.168.12.10');
-		define('MYSQL_USER',   'cncprod');
-		define('MYSQL_PASS',   'cncprod');
-		define('MYSQL_SCHEMA', 'db_cnc');
-		define('MYSQL_PORT',   '3306');
 		define('PROXY', "192.168.12.8"); // usato nelle chiamate al DMS
 		define('PROXYPORT', "3128");
-		define('PORTAL_URL',   'https://portaltest.tfsi.it/');
-		define('LINK_URL',   'https://cnctest.tfsi.it/');
 		define('SMS_TEST_NR','dummy'); // NUMERO DESTINATARIO FISSO PER I TEST (Di Falco)
 		define('MAIL_TEST', "giorgio.difalco@gmail.com"); // indica dove vengono mandati tutti i msg di mail
 		define('MAIL_OVERRIDE','giorgio.difalco@gmail.com');
@@ -67,39 +34,8 @@ switch ($sito) {
 		define('FOOTER','<div style="text-align:center;font-size:10px;color:gray">&copy; 2011-2015 Toyota Financial Services (UK) PLC - P.IVA 05303901002</div>');
 		define('SPECIAL_LINK','DCS.showModificaProvvigione(492466,140946,46)');
 		break;
-	case 'DEMO':
-		define('SITE_NAME', 'DCSys DEMO');
-		define('MYSQL_SERVER', 'localhost');
-		define('MYSQL_USER',   'cncprod');
-		define('MYSQL_PASS',   'cncprod');
-		//define('MYSQL_SCHEMA', 'logica_dcsys'); // versione in chiaro
-		define('MYSQL_SCHEMA', 'dcsys_o');        // versione con dati mascherati
-		define('MYSQL_PORT',   '3306');
-		define('PORTAL_URL',   'http://95.110.157.84/dcsys');
-		define('LINK_URL',   'http://95.110.157.84/dcsys');
-		define('SMS_TEST_NR','dummy'); // no sms
-		define('MAIL_TEST', "g.difalco@logicainformatica.it"); // indica che l'invio mail non viene fatto, in realt�
-		define('MAIL_OVERRIDE','g.difalco@logicainformatica.it');
-		define('ADMIN_MAIL',"g.difalco@logicainformatica.it"); // mail dell'amministratore di sistema
-		define('CERVED_MAIL',"g.difalco@logicainformatica.it"); // mail dell'amministratore di sistema
-		define('MEN_AT_WORK_PAGE',""); // pagina di avviso per interruzione servizio. Se impostata (es. Avviso.html) non fa entrare nell'app ma rimanda ad essa
-		define('TEXT_NEWLINE', "\r\n");  // salto linea per le righe delle lettere formato testo semplice
-		define('FAVICON','favicon.png');
-		define('LOGO_PRODOTTO','<img src="images/logo DCS banner.png">');
-		define('LOGO_SOCIETA','<img src="images/logo LI banner.png">');
-		define('FOOTER','<div style="text-align:center;">&copy; Copyright 2011-2015 Logica Informatica srl - Tutti i diritti riservati</div>');
-		break;
-	case 'PROD':
+	case 'prod':
 		define('SITE_NAME', 'Conn@&cut');
-//		define('MYSQL_SERVER', '77.43.16.59');
-		define('MYSQL_SERVER', '192.168.2.10');
-		define('MYSQL_USER',   'cncprod');
-		define('MYSQL_PASS',   'cncprod');
-		define('MYSQL_SCHEMA', 'db_cnc');
-//		define('MYSQL_PORT',   '3306');
-		define('MYSQL_PORT',   '6446');  // passa attraverso il MySql Monitor
-		define('PORTAL_URL',   'https://portal.tfsi.it/');
-		define('LINK_URL',   'https://cnc.tfsi.it/');
 		define('SMS_TEST_NR',''); // NON PER TEST 
 		define('MAIL_TEST', "");
 		define('MAIL_OVERRIDE','collection@it.toyota-fs.com,francesca.bacci@toyota-fs.com,emanuela.rotondo@toyota-fs.com,CNCITDept@it.toyota-fs.com');
@@ -118,6 +54,15 @@ switch ($sito) {
 		break;
 }
 define('NUM_VERSIONE','1.11.2');
+define('PORTAL_URL',   "{$_SERVER['EB_WEB_PROTOCOL']}://portal{$_SERVER['EB_DNS_SUFFIX']}.{$_SERVER['EB_DNS_DOMAIN']}/");
+define('LINK_URL',   "{$_SERVER['EB_WEB_PROTOCOL']}://cnc{$_SERVER['EB_DNS_SUFFIX']}.{$_SERVER['EB_DNS_DOMAIN']}/");
+
+//mysql
+define('MYSQL_SERVER', $_SERVER['EB_MYSQL_HOST']);//mysql-tfsi-lab.cwikgb27tggo.eu-west-1.rds.amazonaws.com');
+define('MYSQL_USER',  $_SERVER['EB_C_DB_USER']);// 'cnclabit');
+define('MYSQL_PASS',   $_SERVER['EB_C_DB_PASS']); //'cnclabit');
+define('MYSQL_SCHEMA', 'db_cnc');
+define('MYSQL_PORT',   $_SERVER['EB_MYSQL_PORT']); //'3306');
 
 // Ultimo mese di un anno fiscale
 define('LAST_FY_MONTH', "3");  // Marzo
@@ -135,11 +80,11 @@ define('SMS_API','476'); // usare 477 per sms di ritorno
 define('SMS_SENDER','Toyota F.S.');
 
 // Directories
-define('LOG_PATH',dirname(__FILE__).'/../logfiles');	// per i files di traccia
-define('TMP_PATH',dirname(__FILE__).'/../tmp'); 		// per i file di import/export temporanei		
-define('LETTER_PATH',dirname(__FILE__).'/../tmp/lettere'); 		// per i file rotomail
+define('LOG_PATH','/efs/cnc/logfiles');	// per i files di traccia
+define('TMP_PATH','/efs/cnc/tmp'); 		// per i file di import/export temporanei		
+define('LETTER_PATH','/efs/cnc/tmp/lettere'); 		// per i file rotomail
 define('LETTER_URL',LINK_URL.'tmp/lettere'); 		// per i file rotomail
-define('ATT_PATH',dirname(__FILE__).'/../attachments'); // per allegati
+define('ATT_PATH','/efs/cnc/attachments'); // per allegati
 define('REL_PATH', 'attachments'); // inizio URL relativo per allegati
 define('TMP_REL_PATH','tmp');      // inizio URL relativo per file in tmp
 define('TEMPLATE_PATH',dirname(__FILE__).'/../templates'); // per i templates di email, lettere ecc.
@@ -153,7 +98,7 @@ define('DMS_API_KEY','5e884898da28047151d0e56f8dc6292773603d0d6aabbdd62a11ef721d
 
 // Parametri per il funzionamento
 define('MAX_BATCH_ERRORS',9999990);	// numero massimo di errori rilevati prima che un batch termini
-define('MAX_FLAT_MENU',12);	// numero massimo di voci per il menù flat (ad un livello)
+define('MAX_FLAT_MENU',12);	// numero massimo di voci per il menÃ¹ flat (ad un livello)
 
 define('ORA_FINE_GIORNO',20); // ora dopo la quale si considera il giorno dopo per l'affido
 define('GIORNI_CANCELLAZIONE',90); // giorni dopo il quale si cancellano i dati di traccia
@@ -164,7 +109,7 @@ define('GG_ALLA_SCAD_AFF_STR',30); // vengono prelevate le pratiche che hanno un
 define('GG_ALLA_SCAD_RATA_PR',7); // vengono prelevate le pratiche che hanno una data pagamento rata >= alla data attuale + il valore di questo param.
 define('GIORNI_RATA_SCAD',30); // vengono prelevate le pratiche che hanno come ultima data pagamento rata tra la data attuale e la data attuale - il valore di questo param.
 
-define('INACTIVITY_TIMEOUT',1200);    // timeout di inattività in secondi, dopo il quale è richiesto un login
+define('INACTIVITY_TIMEOUT',1200);    // timeout di inattivitÃ  in secondi, dopo il quale Ã¨ richiesto un login
 define('KEEPALIVE_TIME',180); // intervallo di polling per la chiamata di mantenimento in vita della sessione
 define('BACKTRACE', true); 
 define('TRACE',true);   // mettere false per evitare la scrittura su trace.txt
