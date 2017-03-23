2b6034da3c92954561db5eccc14cbe01d6ca44b0
Giorgio Di Falco <g.difalco@logicainformatica.it>
2017-03-23 17:14:41 +0100

    Correzioni v_ultima_azione_utente per mostrare eventi importati

--- sql/VIEW_V_ultima_azione_utente.sql
+++ sql/VIEW_V_ultima_azione_utente.sql
@@ -3,12 +3,13 @@
 #
 create or replace view v_ultima_azione_utente
 AS
-select s.idcontratto,a.titoloazione as UltimaAzione,s.DataEvento as DataUltimaAzione,NomeUtente AS UtenteUltimaAzione,
+select s.idcontratto,IFNULL(a.titoloazione,'') as UltimaAzione,
+s.DataEvento as DataUltimaAzione,NomeUtente AS UtenteUltimaAzione,
 s.NotaEvento
 from storiarecupero s
-join azione a on s.idazione=a.idazione
+LEFT join azione a on s.idazione=a.idazione
 join utente u on u.idutente=s.idutente
-where s.idutente>0 and s.idazione>0 
+where s.idutente>0 and (s.idazione>0 OR DescrEvento>'')
 and not exists (select 1 from storiarecupero x
                 where s.idcontratto=x.idcontratto and x.idutente>0
-                and x.idazione>0 and x.idstoriarecupero>s.idstoriarecupero);
+                and (x.idazione>0 OR DescrEvento>'') and x.idstoriarecupero>s.idstoriarecupero);
