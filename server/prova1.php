ff24dd9c93916db79c03341c7dbb5c4b85cc8845
Giorgio Di Falco <g.difalco@logicainformatica.it>
2017-05-24 15:43:04 +0200

    ELiminazione file dopo report vulnerabilita'

--- server/prova1.php
+++
@@ -1,9 +0,0 @@
-<?php
-$task = "f";
-switch ($task)
-{
-	case "a": echo "1"; break;
-	case ($task<"s"): echo "2"; break;
-	default: echo "3";
-}
-?>
--- server/provaMail.php
+++
@@ -1,9 +0,0 @@
-<?php
-    if (function_exists("mail"))
-    {
-    	$ret = mail("giorgio.difalco@gmail.com","Prova invio mail", "body vuoto", "From: Toyota Financial Services <noreply@tfsi.it>");
-    	echo "Risultato invio mail = $ret";
-    }
-    else
-    	echo "Funzione mail non disponibile";
-?>
