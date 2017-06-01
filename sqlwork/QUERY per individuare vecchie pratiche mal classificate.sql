eed31bcb638603e5d13c845ee0c22ef82ea700d0
Giorgio Di Falco <g.difalco@logicainformatica.it>
2017-06-01 13:02:02 +0200

    allineamento

---
+++ sqlwork/QUERY per individuare vecchie pratiche mal classificate.sql
@@ -0,0 +1,6 @@
+﻿select CodContratto
+ from contratto c where idclasse=18 and idstatorecupero!=13
+and (select sum(importo) from movimento m where m.idcontratto=c.idcontratto)>26
+and CURDATE()-INTERVAL 2 MONTH>(SELECT MAX(LastUpd) FROM movimento x WHERE x.idcontratto=c.idcontratto)
+order by 1
+limit 999999  ;
