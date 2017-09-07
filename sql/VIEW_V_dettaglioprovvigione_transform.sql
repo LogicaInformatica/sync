0479f297aaf1da07b848a2ae7b00fbcb06efc95a
Giorgio Di Falco <g.difalco@logicainformatica.it>
2017-09-07 18:01:04 +0200

    Correzione calcolo provvigioni singola pratica

--- sql/VIEW_V_dettaglioprovvigione_transform.sql
+++ sql/VIEW_V_dettaglioprovvigione_transform.sql
@@ -1,6 +1,7 @@
 ## definisce campi aggiuntivi a dettaglioprovvigione, omonimi dei campi usati nelle formule di calcolo delle provvigioni
 CREATE OR REPLACE VIEW v_dettaglioprovvigione_transform
 AS
+/*
 SELECT IdProvvigione,IdContratto,
   IF (NumRateViaggianti>0,1,0) AS NumViaggianti,
   IFNULL(ImpPagato,0) AS ImpCapitaleIncassato,
@@ -8,3 +9,12 @@ SELECT IdProvvigione,IdContratto,
   IFNULL(ImpSpese,0) AS ImpSpeseRecupero,
   LEAST( ImpCapitaleAffidato, IFNULL(ImpPagato,0) ) AS ImpCapitaleRealeIncassato
  FROM dettaglioprovvigione d
+
+*/
+SELECT IdProvvigione,IdContratto,
+  IF (RateViaggiantiIncassate>0,1,0) AS NumViaggianti,
+  IFNULL(ImpPagato,0) AS ImpCapitaleIncassato,
+  IFNULL(ImpInteressi,0) AS ImpInteressiDiMora,
+  IFNULL(ImpSpese,0) AS ImpSpeseRecupero,
+  LEAST( ImpCapitaleAffidato, IFNULL(ImpPagato,0) ) AS ImpCapitaleRealeIncassato
+ FROM v_dettaglio_provvigioni;
