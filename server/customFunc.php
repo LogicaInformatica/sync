2d8d667d4261abccf1e19a08cc67704a227695bc
unknown <Administrator@.(none)>
2018-01-08 10:06:56 +0100

    Modifiche per visualizzazione liste Non Started

--- server/customFunc.php
+++ server/customFunc.php
@@ -405,7 +405,10 @@ function Custom_List($task,$join,&$query,&$queryForCount,&$fields,&$ordine)
 join insoluto i1 on i1.IdContratto=c.IdContratto AND i1.numRata>0 and i1.DataInsoluto < c.DataDecorrenza+INTERVAL 12 MONTH AND i1.ImpPagato=0
 join insoluto i2 on i2.IdContratto=c.IdContratto AND i2.numRata=i1.NumRata+1 and i2.DataInsoluto < c.DataDecorrenza+INTERVAL 12 MONTH AND i2.ImpPagato=0
 join insoluto i3 on i3.IdContratto=c.IdContratto AND i3.numRata=i2.NumRata+1 and i3.DataInsoluto < c.DataDecorrenza+INTERVAL 12 MONTH AND i3.ImpPagato=0
-where (c.ImpInsoluto > 0 or c.IdStatoRecupero in (79,84)) and c.IdStatoContratto !=29";
+where (c.ImpInsoluto > 0 or c.IdStatoRecupero in (79,84)) and c.IdStatoContratto !=29
+UNION 
+select distinct c.IdContratto from contratto c 
+where c.DataDBT < c.DataDecorrenza + INTERVAL 12 MONTH AND (c.ImpInsoluto > 0 or c.IdStatoRecupero in (79,84)) and c.IdStatoContratto !=29";
             $query =   "v_insoluti_opt v $join WHERE v.IdContratto IN ($subselect)";
 			$queryForCount = $query;
 			break;
