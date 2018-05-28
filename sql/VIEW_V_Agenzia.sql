c2e2aad67200e18462f34b895b6d4d6f5b3e94d7
Giorgio Di Falco <g.difalco@logicainformatica.it>
2018-05-28 10:07:34 +0200

    Correzione view v_agenzia

--- sql/VIEW_V_Agenzia.sql
+++ sql/VIEW_V_Agenzia.sql
@@ -4,4 +4,5 @@ SELECT IdReparto AS IdAgenzia,TitoloUfficio AS TitoloAgenzia,r.*,
 CASE WHEN IdTipoReparto IN (2,3) THEN 'AGE' WHEN IdTipoReparto=4 THEN 'STR' WHEN IdTipoReparto=5 THEN 'LEG' ELSE 'AGE' END AS TipoAgenzia
 FROM reparto r
 WHERE CURDATE() BETWEEN DataIni AND DataFin
-AND IdCompagnia IN (SELECT IdCompagnia FROM compagnia WHERE IdTipoCompagnia=2);
+AND IdTipoReparto  IN (2,3,4,5,6) ;
+ia WHERE IdTipoCompagnia=2);
