fa561c0d28743a7927420f424639e9c0dce235d5
Giorgio Di Falco <g.difalco@logicainformatica.it>
2018-10-31 10:24:04 +0100

    Ottimizzazione view v_lista_telefoni

--- sql/VIEW_V_Lista_telefoni.sql
+++ sql/VIEW_V_Lista_telefoni.sql
@@ -1,7 +1,17 @@
+/*
 CREATE OR REPLACE VIEW v_lista_telefoni
 AS
 SELECT IdCliente,GROUP_CONCAT(Telefono SEPARATOR ', ') AS telefoni
  FROM v_telefoni_e_cellulari
 GROUP BY IdCliente
-
-
+;
+*/
+/** Ottimizzata 2018-10-31 */
+CREATE OR REPLACE VIEW v_lista_telefoni
+AS
+select IdCliente, GROUP_CONCAT(DISTINCT trim(telefono)
+                   ORDER BY CASE WHEN lastuser IN ('import','system') THEN -IdTipoRecapito ELSE IdRecapito END DESC SEPARATOR ', ')
+                  AS telefoni
+from recapito
+where length(trim(telefono))>3
+GROUP BY idcliente;
--- sql/VIEW_V_Telefoni_e_cellulari.sql
+++ sql/VIEW_V_Telefoni_e_cellulari.sql
@@ -1,3 +1,4 @@
+/*
 CREATE OR REPLACE VIEW v_telefoni_e_cellulari
 AS
 select IdCliente,Telefono
@@ -5,3 +6,13 @@ from v_telefono
 UNION
 select IdCliente,Cellulare
 from v_cellulare
+;
+*/
+CREATE OR REPLACE VIEW v_telefoni_e_cellulari
+AS
+select IdCliente, GROUP_CONCAT(DISTINCT trim(telefono)
+                   ORDER BY CASE WHEN lastuser IN ('import','system') THEN -IdTipoRecapito ELSE IdRecapito END DESC SEPARATOR ', ')
+                  AS Telefono
+from recapito
+where length(trim(telefono))>3
+GROUP BY idcliente;
