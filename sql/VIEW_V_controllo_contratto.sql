77dad7236e866d060baac012e2d8a889823b413d
Giorgio Di Falco <g.difalco@logicainformatica.it>
2019-07-11 14:48:26 +0200

    modifica view v_controllo_contratto

--- sql/VIEW_V_controllo_contratto.sql
+++ sql/VIEW_V_controllo_contratto.sql
@@ -1,9 +1,19 @@
 #
 # View usata da Overall per controllare che il numero contratto dato esista e sia a recupero
-#
+# 2019-07-11 left join aggiunta da Claudio De Santis in produzione
 CREATE OR REPLACE VIEW v_controllo_contratto AS
 select count(0) AS QuantiContratti,
-substr(CodContratto,3,(locate('-',concat(CodContratto,'-'))-3)) AS NumContratto,contratto.*
+substr(CodContratto,3,(locate('-',concat(CodContratto,'-'))-3)) AS NumContratto,
+contratto.*,
+`reparto`.`EmailReferente`  AS `EmailReferenteAgenzia`
 from contratto
+LEFT OUTER JOIN `reparto` on `reparto`.`IdReparto` = `contratto`.`IdAgenzia`
 where ImpCapitale>=26 and CodContratto LIKE 'L%'
-group by substr(CodContratto,3)
+group by substr(CodContratto,3);
+
+
+`reparto`.`EmailReferente`  AS `EmailReferenteAgenzia`
+
+from `contratto` LEFT OUTER JOIN `reparto`
+
+on `reparto`.`IdReparto` = `contratto`.`IdAgenzia`
